193f65e26e5a4aab9062998557a99423d7cd679d
Mesel <meselisaac03@hotmail.com>
2019-03-12 02:36:06 +0000

    added errors

--- download_avatars.js
+++ download_avatars.js
@@ -2,8 +2,7 @@ var request = require('request');
 var secrets = require('./secrets');
 var fs = require('fs');
 
-var myArgs1 = process.argv[2]
-var myArgs2 = process.argv[3]
+var myArgs = process.argv.slice(2)
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
@@ -28,8 +27,12 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-getRepoContributors(myArgs1, myArgs2, function(err, result) {
-  console.log("Errors:", err);
+getRepoContributors(myArgs[0], myArgs[1], function(err, result) {
+  if (!myArgs[0] && !myArgs[1]) {
+  console.log("Specify a repoOwner and a repoName ");
+} else if (!myArgs[1]) {
+  console.log("Specify a repoName")
+}
   for (i = 0; i < result.length; i++) {
   downloadImageByURL(result[i].avatar_url, "./avatar/"+result[i].login)
 
